01a7bf239dd20e5ef76e391cbf562c4118e1921c
Daxtresaur <65520387+Daxtresaur@users.noreply.github.com>
2022-06-28 13:47:25 +0300

    Added Prefabs
    - Added prefabs to Player1 & Player2
    
    Other Changes:
    - Removed some scripts (GameLogic)
    - Added code to PathFinding
    - Reset package settings to accomodate change in Unity Version

--- NETWK_MP/Assets/Scripts/PathFinding.cs
+++ NETWK_MP/Assets/Scripts/PathFinding.cs
@@ -25,136 +25,13 @@ public class PathFinding : MonoBehaviour
         if(moveAllowed)
         {
             Move();
+            GameLogic();
+
         }
     }
 
     private void Move()
     {
-
-        //Snakes
-        if (transform.position == wayPoints[26].transform.position)
-        {
-            transform.position = Vector2.MoveTowards(
-            transform.position,
-            wayPoints[4].transform.position,
-            moveSpeed * Time.deltaTime
-            );
-        }
-        else if (transform.position == wayPoints[39].transform.position)
-        {
-            transform.position = Vector2.MoveTowards(
-            transform.position,
-            wayPoints[3].transform.position,
-            moveSpeed * Time.deltaTime
-            );
-        }
-        else if (transform.position == wayPoints[42].transform.position)
-        {
-            transform.position = Vector2.MoveTowards(
-            transform.position,
-            wayPoints[17].transform.position,
-            moveSpeed * Time.deltaTime
-            );
-        }
-        else if (transform.position == wayPoints[53].transform.position)
-        {
-            transform.position = Vector2.MoveTowards(
-            transform.position,
-            wayPoints[30].transform.position,
-            moveSpeed * Time.deltaTime
-            );
-        }
-        else if (transform.position == wayPoints[65].transform.position)
-        {
-            transform.position = Vector2.MoveTowards(
-            transform.position,
-            wayPoints[44].transform.position,
-            moveSpeed * Time.deltaTime
-            );
-        }
-        else if (transform.position == wayPoints[75].transform.position)
-        {
-            transform.position = Vector2.MoveTowards(
-            transform.position,
-            wayPoints[57].transform.position,
-            moveSpeed * Time.deltaTime
-            );
-        }
-        else if (transform.position == wayPoints[88].transform.position)
-        {
-            transform.position = Vector2.MoveTowards(
-            transform.position,
-            wayPoints[52].transform.position,
-            moveSpeed * Time.deltaTime
-            );
-        }
-        else if (transform.position == wayPoints[98].transform.position)
-        {
-            transform.position = Vector2.MoveTowards(
-            transform.position,
-            wayPoints[40].transform.position,
-            moveSpeed * Time.deltaTime
-            );
-        }
-
-        //Ladders
-        if (transform.position == wayPoints[4].transform.position)
-        {
-            transform.position = Vector2.MoveTowards(
-            transform.position,
-            wayPoints[24].transform.position,
-            moveSpeed * Time.deltaTime
-            );
-        }
-        else if (transform.position == wayPoints[12].transform.position)
-        {
-            transform.position = Vector2.MoveTowards(
-            transform.position,
-            wayPoints[45].transform.position,
-            moveSpeed * Time.deltaTime
-            );
-        }
-        else if (transform.position == wayPoints[32].transform.position)
-        {
-            transform.position = Vector2.MoveTowards(
-            transform.position,
-            wayPoints[48].transform.position,
-            moveSpeed * Time.deltaTime
-            );
-        }
-        else if (transform.position == wayPoints[49].transform.position)
-        {
-            transform.position = Vector2.MoveTowards(
-            transform.position,
-            wayPoints[68].transform.position,
-            moveSpeed * Time.deltaTime
-            );
-        }
-        else if (transform.position == wayPoints[41].transform.position)
-        {
-            transform.position = Vector2.MoveTowards(
-            transform.position,
-            wayPoints[62].transform.position,
-            moveSpeed * Time.deltaTime
-            );
-        }
-        else if (transform.position == wayPoints[61].transform.position)
-        {
-            transform.position = Vector2.MoveTowards(
-            transform.position,
-            wayPoints[80].transform.position,
-            moveSpeed * Time.deltaTime
-            );
-        }
-        else if (transform.position == wayPoints[73].transform.position)
-        {
-            transform.position = Vector2.MoveTowards(
-            transform.position,
-            wayPoints[91].transform.position,
-            moveSpeed * Time.deltaTime
-            );
-        }
-
 
         if (waypointIndex <= wayPoints.Length - 1)
         {
@@ -171,6 +48,134 @@ public class PathFinding : MonoBehaviour
                 waypointIndex += 1;
             }
 
+        }
+    }
+
+    private void GameLogic()
+    {
+
+        //Snakes
+        if (transform.position == wayPoints[26].transform.position)
+        {
+            transform.position = Vector2.MoveTowards(
+            wayPoints[4].transform.position,
+            wayPoints[4].transform.position,
+            moveSpeed * Time.deltaTime
+            );
+        }
+        else if (transform.position == wayPoints[39].transform.position)
+        {
+            transform.position = Vector2.MoveTowards(
+            wayPoints[3].transform.position,
+            wayPoints[3].transform.position,
+            moveSpeed * Time.deltaTime
+            );
+        }
+        else if (transform.position == wayPoints[42].transform.position)
+        {
+            transform.position = Vector2.MoveTowards(
+            wayPoints[17].transform.position,
+            wayPoints[17].transform.position,
+            moveSpeed * Time.deltaTime
+            );
+        }
+        else if (transform.position == wayPoints[53].transform.position)
+        {
+            transform.position = Vector2.MoveTowards(
+            wayPoints[30].transform.position,
+            wayPoints[30].transform.position,
+            moveSpeed * Time.deltaTime
+            );
+        }
+        else if (transform.position == wayPoints[65].transform.position)
+        {
+            transform.position = Vector2.MoveTowards(
+            wayPoints[44].transform.position,
+            wayPoints[44].transform.position,
+            moveSpeed * Time.deltaTime
+            );
+        }
+        else if (transform.position == wayPoints[75].transform.position)
+        {
+            transform.position = Vector2.MoveTowards(
+            wayPoints[57].transform.position,
+            wayPoints[57].transform.position,
+            moveSpeed * Time.deltaTime
+            );
+        }
+        else if (transform.position == wayPoints[88].transform.position)
+        {
+            transform.position = Vector2.MoveTowards(
+            wayPoints[52].transform.position,
+            wayPoints[52].transform.position,
+            moveSpeed * Time.deltaTime
+            );
+        }
+        else if (transform.position == wayPoints[98].transform.position)
+        {
+            transform.position = Vector2.MoveTowards(
+            wayPoints[40].transform.position,
+            wayPoints[40].transform.position,
+            moveSpeed * Time.deltaTime
+            );
+        }
+
+        //Ladders
+        if (transform.position == wayPoints[4].transform.position)
+        {
+            transform.position = Vector2.MoveTowards(
+            wayPoints[24].transform.position,
+            wayPoints[24].transform.position,
+            moveSpeed * Time.deltaTime
+            );
+        }
+        else if (transform.position == wayPoints[12].transform.position)
+        {
+            transform.position = Vector2.MoveTowards(
+            wayPoints[45].transform.position,
+            wayPoints[45].transform.position,
+            moveSpeed * Time.deltaTime
+            );
+        }
+        else if (transform.position == wayPoints[32].transform.position)
+        {
+            transform.position = Vector2.MoveTowards(
+            wayPoints[48].transform.position,
+            wayPoints[48].transform.position,
+            moveSpeed * Time.deltaTime
+            );
+        }
+        else if (transform.position == wayPoints[49].transform.position)
+        {
+            transform.position = Vector2.MoveTowards(
+            wayPoints[68].transform.position,
+            wayPoints[68].transform.position,
+            moveSpeed * Time.deltaTime
+            );
+        }
+        else if (transform.position == wayPoints[41].transform.position)
+        {
+            transform.position = Vector2.MoveTowards(
+            wayPoints[62].transform.position,
+            wayPoints[62].transform.position,
+            moveSpeed * Time.deltaTime
+            );
+        }
+        else if (transform.position == wayPoints[61].transform.position)
+        {
+            transform.position = Vector2.MoveTowards(
+            wayPoints[80].transform.position,
+            wayPoints[80].transform.position,
+            moveSpeed * Time.deltaTime
+            );
+        }
+        else if (transform.position == wayPoints[73].transform.position)
+        {
+            transform.position = Vector2.MoveTowards(
+            wayPoints[91].transform.position,
+            wayPoints[91].transform.position,
+            moveSpeed * Time.deltaTime
+            );
         }
     }
 }
